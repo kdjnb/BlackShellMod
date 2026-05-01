d883349b2e917eb34e66b8c6ed738761d3f90991
Nacho <kyuharu@qq.com>
2026-05-01 12:25:14 +0800

    feat: replace HintBlackShellUser with TagIcon and refactor message listeners
    - Remove `HintBlackShellUser` hook and its associated shortcut menu logic
    - Implement `TagIcon` (Birthmark) API hook to inject custom icons into message bubbles based on bubble attributes
    - Update `QQSendMsgListener` to support `MsgAttributeInfo` interception via new listeners
    - Generalize `QQMessageViewListener` to support generic `BaseHookItem` types, allowing non-switchable hooks to listen for view updates
    - Clean up `BottomShortcutMenu` by removing deleted feature entries

--- app/src/main/java/moe/ono/hooks/base/api/QQMessageViewListener.kt
+++ app/src/main/java/moe/ono/hooks/base/api/QQMessageViewListener.kt
@@ -1,34 +1,19 @@
 package moe.ono.hooks.base.api
 
-import android.content.Context
-import android.os.Bundle
-import android.util.Log
 import android.view.View
 import com.tencent.qqnt.kernel.nativeinterface.MsgRecord
-import de.robv.android.xposed.XposedBridge
 import moe.ono.bridge.kernelcompat.ContactCompat
-import moe.ono.config.ConfigManager
 import moe.ono.config.ONOConf
-import moe.ono.constants.Constants
 import moe.ono.hooks._base.ApiHookItem
+import moe.ono.hooks._base.BaseHookItem
 import moe.ono.hooks._base.BaseSwitchFunctionHookItem
 import moe.ono.hooks._core.annotation.HookItem
 import moe.ono.hooks._core.factory.ExceptionFactory
-import moe.ono.hooks._core.factory.HookItemFactory
-import moe.ono.hooks.item.chat.SelfMessageReactor
-import moe.ono.hooks.protocol.buildMessage
-import moe.ono.hooks.protocol.sendPacket
-import moe.ono.hostInfo
 import moe.ono.reflex.ClassUtils
 import moe.ono.reflex.FieldUtils
-import moe.ono.reflex.Ignore
 import moe.ono.reflex.MethodUtils
-import moe.ono.service.QQInterfaces
-import moe.ono.util.Initiator.loadClass
 import moe.ono.util.Logger
 import moe.ono.util.QAppUtils
-import java.lang.ref.WeakReference
-import java.lang.reflect.Method
 
 
 @HookItem(path = "API/监听QQMsgView更新")
@@ -37,7 +22,7 @@ class QQMessageViewListener : ApiHookItem() {
 
     companion object {
 
-        private val ON_AIO_CHAT_VIEW_UPDATE_LISTENER_MAP: HashMap<BaseSwitchFunctionHookItem, OnChatViewUpdateListener> =
+        private val ON_AIO_CHAT_VIEW_UPDATE_LISTENER_MAP: HashMap<BaseHookItem, OnChatViewUpdateListener> =
             HashMap()
 
         /**
@@ -45,7 +30,7 @@ class QQMessageViewListener : ApiHookItem() {
          */
         @JvmStatic
         fun addMessageViewUpdateListener(
-            hookItem: BaseSwitchFunctionHookItem,
+            hookItem: BaseHookItem,
             onMsgViewUpdateListener: OnChatViewUpdateListener
         ) {
             ON_AIO_CHAT_VIEW_UPDATE_LISTENER_MAP[hookItem] = onMsgViewUpdateListener
@@ -85,7 +70,15 @@ class QQMessageViewListener : ApiHookItem() {
 
 
         for ((switchFunctionHookItem, listener) in ON_AIO_CHAT_VIEW_UPDATE_LISTENER_MAP.entries) {
-            if (switchFunctionHookItem.isEnabled) {
+            if (switchFunctionHookItem is BaseSwitchFunctionHookItem) {
+                if (switchFunctionHookItem.isEnabled) {
+                    try {
+                        listener.onViewUpdateAfter(msgView, msgRecord)
+                    } catch (e: Throwable) {
+                        ExceptionFactory.add(switchFunctionHookItem, e)
+                    }
+                }
+            } else {
                 try {
                     listener.onViewUpdateAfter(msgView, msgRecord)
                 } catch (e: Throwable) {
